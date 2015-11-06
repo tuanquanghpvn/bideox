2405dd4bbdbe38e053a2fea4d35f5ebca7e6bb3a
Truong Tuan Quang <truong.tuan.quang@framgia.com>
2015-11-06 16:47:00 +0700

    [Add] user profile

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,
+                                primary_key=True, related_name='profile')
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        verbose_name = 'UserProfile'
+        verbose_name_plural = 'UserProfiles'
+        db_table = 'user_profile'
+
+
